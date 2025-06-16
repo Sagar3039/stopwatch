060b6e70356c50e2c4c8551c646aa5cccac38922
Sagar Karmakar <92040943+Sagar3039@users.noreply.github.com>
2025-06-16 17:19:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let sec = document.getElementById("sec");
 let minute = document.getElementById("minute");
 let hour = document.getElementById("hour");
 let reset = document.getElementById("Reset");
-let mili = document.getElementById("mili")
+let mili = document.getElementById("mili");
 
 let intervalId;
 let isRunning = false;
@@ -14,62 +14,34 @@ ok.addEventListener("click", () => {
     ok.innerText = "Stop";
 
     intervalId = setInterval(() => {
-      let currentMili= parseInt(mili.innerText);
+      let currentMili = parseInt(mili.innerText);
       let currentSec = parseInt(sec.innerText);
       let currentMin = parseInt(minute.innerText);
       let currentHour = parseInt(hour.innerText);
 
       currentMili++;
       if (currentMili === 100) {
-        currentSec++;
         currentMili = 0;
-        
+        currentSec++;
       }
 
       if (currentSec === 60) {
         currentSec = 0;
         currentMin++;
-        currentMili = 0;
       }
+
       if (currentMin === 60) {
-        currentSec = 0;
-        currentMin=0;
-        currentMili = 0;
+        currentMin = 0;
         currentHour++;
       }
-      if (currentMili<10) {
-        mili.innerText = "0"+currentMili
-      }
-      else{
-        mili.innerText = currentMili
-      }
-      if (currentSec<10) {
-        sec.innerText = "0"+currentSec
-      }
-      else{
-        sec.innerText = currentSec
-      }
-      if (currentMin<10) {
-        minute.innerText = "0"+currentMin
-      }
-      else{
-        minute.innerText = currentMin
-      }
-      if (currentHour<10) {
-        hournnerText = "0"+currentHour
-      }
-      else{
-        hour.innerText = currentHour
-      }
-      // mili.innerText= currentMili;
-      // sec.innerText = String(currentSec).padStart(2, "0");
-      // minute.innerText = String(currentMin).padStart(2, "0");
-      // hour.innerText = String(currentHour).padStart(2, "0");
-    }, 1);
 
+      mili.innerText = currentMili < 10 ? "0" + currentMili : currentMili;
+      sec.innerText = currentSec < 10 ? "0" + currentSec : currentSec;
+      minute.innerText = currentMin < 10 ? "0" + currentMin : currentMin;
+      hour.innerText = currentHour < 10 ? "0" + currentHour : currentHour;
+    }, 10); // use 10ms for better performance
   } else {
     clearInterval(intervalId);
-    intervalId = null;
     isRunning = false;
     ok.innerText = "Start";
   }
@@ -79,9 +51,11 @@ reset.addEventListener("click", () => {
   clearInterval(intervalId);
   intervalId = null;
   isRunning = false;
-  mili.innerText ="00";
+
+  mili.innerText = "00";
   sec.innerText = "00";
   minute.innerText = "00";
-  hour.innerText ="00";
+  hour.innerText = "00";
+
   ok.innerText = "Start";
 });
